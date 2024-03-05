e6f31ae8130a3a778e2fa0f3bd6a1f0c20cae7a9
pyae phyo <pyaephyo34@gmail.com>
2024-03-05 13:24:55 +0630

    update

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const express=require('express')
+const express = require('express');
+const app = express();
 
-const app = express()
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
 
-app.get('/', (req,res)=>{
-
-    res.send({hi:'there'})
-
-})
-
-const port = process.env.PORT || 3000
-app.listen(3000)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
